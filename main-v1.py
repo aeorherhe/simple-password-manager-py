e1ca308fe2ceaaa91e47670f90a495848e57135a
Abraham Orherhe <aeorherhe@icloud.com>
2024-08-18 18:10:08 +0100

    add search funtionality

--- main-v1.py
+++ main-v1.py
@@ -1,4 +1,3 @@
-import json
 from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
@@ -39,45 +38,17 @@ def save_pass():
     email = email_entry.get()
     user_password = password_entry.get()
 
-    new_data = {website: {'email': email, 'password': user_password}}
-
     if len(website) == 0 or len(user_password) == 0:
         messagebox.showinfo(title="Error", message="Please enter all values")
     else:
-        try:
-            with open('data.json', 'r') as pass_saved:
-                prev_data = json.load(pass_saved)
-        except FileNotFoundError:
-            with open('data.json', 'w') as pass_saved:
-                json.dump(new_data, pass_saved, indent=4)
-        else:
-            # update previous data in database
-            prev_data.update(new_data)
+        user_resp = messagebox.askokcancel(title=website, message=f"Email: {email}\nPassword: {user_password}\n\n"
+                                                                  f"Click Ok to continue or Cancel to amend")
 
-            # re-open file and write the update file to save
-            with open('data.json', 'w') as pass_saved:
-                json.dump(prev_data, pass_saved, indent=4)
-        finally:
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
-
-
-# ---------------------------- SEARCH PASSWORD ------------------------------- #
-
-def search_db():
-    website = website_entry.get()
-    try:
-        with open('data.json') as pass_saved:
-            data_db = json.load(pass_saved)
-    except FileNotFoundError:
-        messagebox.showinfo(title='Error', message='No password found')
-    else:
-        if website in data_db:
-            result = data_db[website]
-            # display search result to user
-            messagebox.showinfo(title=website, message=f"Email: {result['email']}\nPassword: {result['password']}")
-        else:
-            messagebox.showinfo(title='Error', message=f"No details found for {website}")
+        if user_resp:
+            with open('data.txt', 'a') as pass_saved:
+                pass_saved.write(f"{website} | {email} | {user_password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -100,26 +71,23 @@ email_label.grid(column=0, row=2)
 password_label = Label(text="Password")
 password_label.grid(column=0, row=3)
 
-gen_btn = Button(text="Generate Password", command=gen_password, width=15)
-gen_btn.grid(column=2, row=3)
+gen_btn_label = Button(text="Generate Password", command=gen_password)
+gen_btn_label.grid(column=2, row=3)
 
-add_btn = Button(text="add", width=35, command=save_pass)
-add_btn.grid(column=1, row=4, columnspan=2)
-
-search_btn = Button(text='Search', width=15, command=search_db)
-search_btn.grid(column=2, row=1)
+add_btn_label = Button(text="add", width=35, command=save_pass)
+add_btn_label.grid(column=1, row=4, columnspan=2)
 
 # ------------------ Inputs ----------------- #
 
-website_entry = Entry(width=18)
-website_entry.grid(column=1, row=1)
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
 website_entry.focus()
 
-email_entry = Entry(width=38)
+email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
 email_entry.insert(0, 'example@test.com')
 
-password_entry = Entry(width=18)
+password_entry = Entry(width=20)
 password_entry.grid(column=1, row=3)
 
 
